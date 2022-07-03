f92d91da8f419bd679e06a99c36a12d816ae68e2
Horwell <horwellmv@gmail.com>
2022-07-02 23:48:33 -0300

    Creacion de Componentes Navbar y componentes de navegacion, home, proyectos, experiencia, educacion, skills... y configuracion de archivo de rutas.

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './Componentes/home/home.component';
+import { EducacionComponent } from './Componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './Componentes/experiencia/experiencia.component';
+import { ProyectosComponent } from './Componentes/proyectos/proyectos.component';
+import { SkillsComponent } from './Componentes/skills/skills.component';
+
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "educacion", component: EducacionComponent},
+  {path: "experiencia", component: ExperienciaComponent},
+  {path: "proyectos", component: ProyectosComponent},
+  {path: "skills", component: SkillsComponent},
+  {path: "**", component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,9 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Servicios
+
+//Componentes
+import { NavbarComponent } from './Componentes/Compartidos/navbar/navbar.component';
+import { HomeComponent } from './Componentes/home/home.component';
+import { EducacionComponent } from './Componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './Componentes/experiencia/experiencia.component';
+import { ProyectosComponent } from './Componentes/proyectos/proyectos.component';
+import { SkillsComponent } from './Componentes/skills/skills.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,

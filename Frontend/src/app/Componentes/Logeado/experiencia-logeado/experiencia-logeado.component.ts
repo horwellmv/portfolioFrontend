6b86d27462c0d0e0980780504ea9bd65450bf89d
Horwell <horwellmv@gmail.com>
2022-10-31 23:21:27 -0300

    se crea experiencia component para editar

---
+++ Frontend/src/app/Componentes/Logeado/experiencia-logeado/experiencia-logeado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencia-logeado',
+  templateUrl: './experiencia-logeado.component.html',
+  styleUrls: ['./experiencia-logeado.component.css']
+})
+export class ExperienciaLogeadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

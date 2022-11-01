a1c95e9403ac0bd59b78d96d53dbf08ea01efb33
Horwell <horwellmv@gmail.com>
2022-10-31 23:22:03 -0300

    se crea home component para carga de datos

---
+++ Frontend/src/app/Componentes/Logeado/home-logeado/home-logeado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-logeado',
+  templateUrl: './home-logeado.component.html',
+  styleUrls: ['./home-logeado.component.css']
+})
+export class HomeLogeadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
